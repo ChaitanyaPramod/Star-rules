b003d6a957cbcd2bbc279ebc61b5f7c78d1ae280
Chaitanya Pramod <me@chaitanyapramod.com>
2013-06-16 15:07:06 +0530

    Update manifest_version

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-29329416-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+
 // enabled[tab.id] = true|false
 var enabled = {};
 
